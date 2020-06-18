9394a6566ec50e58e38e7bfe8fea42ea6975b794
Kirill Menshov <kirill.menshov@gmail.com>
2020-06-18 16:00:20 +0300

    authorization with passport

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,17 @@
+import { Matches, IsString, MinLength, MaxLength } from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Password must include at least one number, one capital letter and one special character',
+  })
+  password: string;
+}
