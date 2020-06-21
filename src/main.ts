a17f9f875c08069af4ddf2b6391c59ff897d9ee8
Kirill Menshov <kirill.menshov@gmail.com>
2020-06-21 20:33:46 +0300

    config added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
-  const port = 3000;
   const logger = new Logger('bootstrap');
+
+  const serverConfig = config.get('server');
+  const port = process.env.PORT || serverConfig.port;
 
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
